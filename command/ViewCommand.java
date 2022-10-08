1c6229a12c6a4c86fc7c64ca475fa074569a2dbe
Jackson Weems <jtweems@email.sc.edu>
2022-10-08 15:48:18 -0400

    adding view command

--- command/ViewCommand.java
+++ command/ViewCommand.java
@@ -1,12 +1,21 @@
 package command;
-
+/**
+ * a view command
+ * @author jackson weems
+ */
 public class ViewCommand extends Command {
-    
+    /**
+     * creates the view command
+     * @param doc where the data is viewed
+     */
     public ViewCommand(Document doc) {
-        // add stuff
+        super(doc);
     }
-
+    /**
+     * gets a String version of the document
+     * @return data in the document
+     */
     public String execute() {
-        return ""; // change
+        return Document.view(); 
     }
 }
