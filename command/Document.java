f3d13e9bc153fc07019b763b5d5b7939b591c760
Carter-Risse <crisse@email.sc.edu>
2022-10-08 14:50:03 -0400

    Adding bare class

--- command/Document.java
+++ command/Document.java
@@ -1,5 +1,28 @@
 package command;
 
+import java.util.ArrayList;
+
 public class Document {
+public String fileName;
+public ArrayList<String> lines;
+    public Document(String docName) {
+
+    }
+    public String view()
+    {
+
+    }
+    public String append(String line)
+    {
+
+    }
+    public String write(String line)
+    {
+
+    }
+    public String save()
+    {
+        
+    }
 
 }
