e3e0dab4796fe8840ada18f7499f9f465ef197cb
Tyler Misyak <tymisyak@gmail.com>
2022-10-09 18:18:45 -0400

    adding InputHandler and Command

--- command/Command.java
+++ command/Command.java
@@ -1,11 +1,24 @@
 package command;
 
+/**
+ * A command
+ * @author Misyak
+ */
 public abstract class Command {
 
     protected Document document;
 
+    /**
+     * Creates a command
+     * @param document Where the data will be affected by the commands
+     */
     public Command(Document document) {
         this.document = document;
     }
+
+    /**
+     * Execution of the specific command
+     * @return A string of the data in the document
+     */
     public abstract String execute();
 }
--- command/InputHandler.java
+++ command/InputHandler.java
@@ -1,17 +1,28 @@
 package command;
 
 import java.util.HashMap;
-
+/**
+ * A class that handles what the user inputs
+ * @author Misyak
+ */
 public class InputHandler {
     private HashMap<String, Command> commands;
 
+    /**
+     * Creates the away to handle inputs by adding commands and strings to a HashMap
+     * @param document The document where data will be stored
+     */
     public InputHandler(Document document) {
-        commands.put("save", SaveCommand(document));
-        commands.put("view", ViewCommand(document));
-        commands.put("append", AppendCommand(document));
-        commands.put("write", WriteCommand(document));
+        commands.put("save", new SaveCommand(document));
+        commands.put("view", new ViewCommand(document));
+        commands.put("append", new AppendCommand(document));
+        commands.put("write", new WriteCommand(document));
     }
 
+    /**
+     * Check if the key data is in our HashMap
+     * @param data A string of what command the user entered
+     */
     public void inputEntered(String data) {
         commands.get(data);
     }
