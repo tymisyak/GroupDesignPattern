b1d6c9a0967d6216f9da1570cc9f3c769b5ae82f
MJYassine <74205152+MJYassine@users.noreply.github.com>
2022-10-09 19:45:38 -0400

    Add files via upload

--- command/AppendCommand.java
+++ command/AppendCommand.java
@@ -2,16 +2,29 @@ package command;
 
 import java.util.Scanner;
 
+/**
+ * a append command
+ * 
+ * @author Mehdi Yassine
+ */
 public class AppendCommand extends Command {
-    
     private Scanner in;
 
+    /**
+     * creates the view command
+     * 
+     * @param doc where the data is viewed
+     */
     public AppendCommand(Document doc) {
-        // add stuff
+        super(doc);
     }
 
+    /**
+     * gets a String version of the document
+     * 
+     * @return data in the document
+     */
     public String execute() {
-        return ""; // change
+        return "The line has been appended to the document";
     }
-
 }
--- command/WriteCommand.java
+++ command/WriteCommand.java
@@ -2,16 +2,31 @@ package command;
 
 import java.util.Scanner;
 
+/**
+ * a write command
+ * 
+ * @author Mehdi Yassine
+ */
 public class WriteCommand extends Command {
-    
+
     private Scanner in;
 
+    /**
+     * creates the write command
+     * 
+     * @param doc where the data is viewed
+     */
     public WriteCommand(Document doc) {
-        // add stuff
+        super(doc);
     }
 
+    /**
+     * gets a String version of the document
+     * 
+     * @return data in the document
+     */
     public String execute() {
-        return ""; // change
+        return "The line was written to the file";
     }
-    
+
 }
